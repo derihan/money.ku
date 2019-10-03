4ce19add78b1ee6c683de8766726c72888114d63
derihan <45185667+derihan@users.noreply.github.com>
2019-10-03 09:00:37 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
+
+// akses Cors importan !
+
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
+// end cors
+
 use Illuminate\Http\Request;
 
 /*
